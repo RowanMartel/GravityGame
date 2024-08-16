a495b11c9329227952c066fc7ee54c3716d19d96
Rowan Martel <92890554+NotEnoughUwU@users.noreply.github.com>
2024-08-16 01:12:22 -0300

    got controls down

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
     JumpDir jumpDir;
 
     void Update()
-    {
+    {/*
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         if (Input.GetButtonDown("JumpLeft"))
@@ -30,6 +30,65 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpDir = JumpDir.right;
             jump = true;
+        }*/
+        switch (GravityManager.instance.gravityDir)
+        {
+            case GravityManager.GravityDir.down:
+                horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+                if (Input.GetKeyDown(KeyCode.JoystickButton0))
+                {
+                    jumpDir = JumpDir.left;
+                    jump = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.JoystickButton2))
+                {
+                    jumpDir = JumpDir.right;
+                    jump = true;
+                }
+                break;
+            case GravityManager.GravityDir.up:
+                horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+                if (Input.GetKeyDown(KeyCode.JoystickButton2))
+                {
+                    jumpDir = JumpDir.left;
+                    jump = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.JoystickButton0))
+                {
+                    jumpDir = JumpDir.right;
+                    jump = true;
+                }
+                break;
+            case GravityManager.GravityDir.left:
+                horizontalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+
+                if (Input.GetKeyDown(KeyCode.JoystickButton3))
+                {
+                    jumpDir = JumpDir.left;
+                    jump = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.JoystickButton1))
+                {
+                    jumpDir = JumpDir.right;
+                    jump = true;
+                }
+                break;
+            case GravityManager.GravityDir.right:
+                horizontalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+
+                if (Input.GetKeyDown(KeyCode.JoystickButton1))
+                {
+                    jumpDir = JumpDir.left;
+                    jump = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.JoystickButton3))
+                {
+                    jumpDir = JumpDir.right;
+                    jump = true;
+                }
+                break;
         }
     }
 
